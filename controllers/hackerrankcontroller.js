1db4cd7fb4393b66bd0bdb76a2c76b3f51494589
21r21a33333 <21r21a3333@mlrinstitutions.ac.in>
2024-03-18 21:16:39 +0530

    hackerrank route change

--- controllers/hackerrankcontroller.js
+++ controllers/hackerrankcontroller.js
@@ -2,8 +2,7 @@
 const axios = require('axios');
 const hackerrankstat=async (req,res)=>{
     try{
-        let data = await axios.get(`https://www.hackerrank.com/profile/bhargavdh5`);
-        res.send(data.data);
+        res.send(data.test);
     }catch(err){
         res.json({error:err});
     }
