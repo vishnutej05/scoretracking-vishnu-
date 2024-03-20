53a16867b12bd88ba5cfff3f992cb8b4b6b41073
21r21a33333 <21r21a3333@mlrinstitutions.ac.in>
2024-03-20 19:16:30 +0530

    minor change

--- models/leaderboard.js
+++ models/leaderboard.js
@@ -3,12 +3,12 @@ const mongoose=require("mongoose");
 const leaderboardSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     roll_no: { type: String, required: true },
-    lc_leaderboard_score: { type: String, required: true },
     lc_leaderboard_score: { type: Number, default: 0 },
     cc_leaderboard_score: { type: Number, default: 0 },
     cf_leaderboard_score: { type: Number, default: 0 },
     hr_leaderboard_score: { type: Number, default: 0 },
     spoj_leaderboard_score: { type: Number, default: 0 },
+    coursework_score: { type: Number, required: true },
     total_leaderboard_score: { type: Number, default: 0 },
 });
 
