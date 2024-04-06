f5ff866509a8da2dc256708312666e0436f8acfd
vishnutej05 <tejvishnu05@gmail.com>
2024-04-06 15:30:25 +0530

    to deploy

--- dbconfig.js
+++ dbconfig.js
@@ -1,12 +1,13 @@
-const mongoose=require("mongoose")
-const {MONGO_URL}=require("./config")
-async function databaseconnect(){
-    try{
-        console.log("trying to connect");
-        let data=await mongoose.connect(MONGO_URL);
-        console.log("connected to mongo");
-    }catch(err){
-        console.log(err)
-    }
+const mongoose = require("mongoose");
+require("dotenv").config();
+// const {MONGO_URL}=require("./config")
+async function databaseconnect() {
+  try {
+    console.log("trying to connect");
+    let data = await mongoose.connect(process.env.MONGO_URL);
+    console.log("connected to mongo");
+  } catch (err) {
+    console.log(err);
+  }
 }
-module.exports={databaseconnect};
+module.exports = { databaseconnect };
